376a77add670737c95fee47115b15703e276f913
René Fritze <rene.fritze@wwu.de>
2023-02-12 21:04:30 +0100

    add typer

--- src/pdf_slicer/gui/__main__.py
+++ src/pdf_slicer/gui/__main__.py
@@ -3,10 +3,5 @@ import typer
 from pdf_slicer.gui.window import run
 
 
-def main():
-    """Entry point for the application script."""
-    return typer.run(run)
-
-
 if __name__ == "__main__":
     typer.run(run)
